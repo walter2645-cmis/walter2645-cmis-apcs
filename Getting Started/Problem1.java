8dd570cc75f2dbb751af50e5afc449d1a07d13ca
cmis <cmis@cmis-hs08.(none)>
2016-08-09 11:56:02 +0700

    Created Getting Started project. Finished(?) 2 problems

---
+++ Getting Started/Problem1.java
@@ -0,0 +1,7 @@
+//This prints out a string
+public class Problem1 {
+    public static void main (String args[] )
+    {
+        System.out.println("Hello, World!");
+    } //end method main
+} //end class Problem1
---
+++ Getting Started/Problem2.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+//This is user input?
+public class Problem2
+    {
+    public static void main( String[] args )
+        {
+        String input = JOptionPane.showInputDialog( "What is your name?");
+        
+        System.out.println("Hello," + input );
+        } //end method main
+    } //end class Problem2
