aa6c73e9a1018113fc7ca73671bb94b7767c934c
cmis <cmis@cmis-hs07.(none)>
2016-08-15 09:56:46 +0700

    Created Fundamentals project. Started on Problem0.

---
+++ Fundamentals/Problem0.java
@@ -0,0 +1,34 @@
+public class Problem0 {
+    public static void main ( String[] args )
+    {
+        byte a = 0;
+        byte b = 69;
+        short c = 1928;
+        short d = 3;
+        int e = 123456789;
+        int f = 169 * -169;
+        long g = f * 169284;
+        long h = g * 169284;
+        float i = 1.1f;
+        float j = 2.345678f;
+        double k = 3.1415926535123546585;
+        double l = 212121212121.212121;
+        boolean m = true;
+        boolean n = false;
+        char o = 'A';
+        char p = 65535;
+        System.out.println("a byte is used to conserve space and stores an integer between -128 and 127, such as " + a);
+        System.out.println("a byte is used to conserve space and stores an integer between -128 and 127, such as " + b);
+        System.out.println("a short contains more data than a byte and allows the integer value range to be increased from -32768 to 32767. an example is " + c);
+        System.out.println("a short contains more data than a byte and allows the integer value range to be increased from -32768 to 32767. an example is " + d);
+        System.out.println("an int is the standard storage type for integers, and stores an integer ranging from -2147483648 to 2147483647. an example is " + e);
+        System.out.println("an int is the standard storage type for integers, and stores an integer ranging from -2147483648 to 2147483647. an example is " + f);
+        System.out.println("a long is for storing numbers that are abnormally long, ranging from -9223372036854775808 to 9,223,372,036,854,775,807 . an example is " + g);
+        System.out.println("a long is for storing numbers that are abnormally long, ranging from -9223372036854775808 to 9,223,372,036,854,775,807 . an example is " + h);
+        System.out.println("a float is for storing decimals up to 7 places and an example is" + i);
+        System.out.println("a float is for storing decimals up to 7 places and an example is" + j);
+        System.out.println("a double is for storing decimals up to 16 places, and an example is " + k);
+        System.out.println("a double is for storing decimals up to 16 places, and an example is " + l);
+        System.out.println("a boolean is either true or false");
+    }//end method main
+}//end class Problem1
