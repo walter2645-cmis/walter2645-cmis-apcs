48d72e36f106474cc7c80e4f9f0154abc1027050
cmis <cmis@cmis-hs03.(none)>
2016-08-24 08:41:42 +0700

    Edited tester for traversing an array from back to front.

--- Fundamentals/tester.java
+++ Fundamentals/tester.java
@@ -2,9 +2,10 @@ public class tester
 {
     public static void main( String[] args )
     {
-        double asd = Math.random() * 3.3;
-        System.out.println(asd);
-        double def = 1.0 / 3;
-        System.out.println(def);
+        int[] asd = new int[99];
+        for( int index = asd.length - 1; index > 0; index-- )
+        {
+            System.out.println(asd[ index ]);
+        }//end for
     }//end method main
 }//end class tester
