3de982720529c629ea4c8e6eeb9282c898069e77
cmis <cmis@cmis-hs03.(none)>
2016-08-25 09:56:15 +0700

    Used tester class to do a problem of sorting a number. Completed errors and WDID.

---
+++ Fundamentals/ThisGame.java
@@ -0,0 +1,7 @@
+public class ThisGame
+{
+    public static void main( String[] args )
+    {
+        
+    }//end method main
+}//end class ThisGame
--- Fundamentals/tester.java
+++ Fundamentals/tester.java
@@ -2,10 +2,23 @@ public class tester
 {
     public static void main( String[] args )
     {
-        int[] asd = new int[99];
-        for( int index = asd.length - 1; index > 0; index-- )
+        int[] asd = new int[1000000];
+        for( int index = 0; index < asd.length; index++ )
         {
-            System.out.println(asd[ index ]);
+            asd[ index ] = (int)(Math.random() * 1000000);
+        }//end for
+        boolean confirm = true;
+        for( int index = 0; confirm; index++ )
+        {
+            boolean three = asd[ index ] % 3 == 0;
+            boolean five = asd[ index ] % 5 == 0;
+            boolean seven = asd[ index ] % 7 == 0;
+            boolean eleven = asd[ index ] % 11 == 0;
+            if( three && five && seven && eleven )
+            {
+                System.out.println(asd[ index ] + " " + index);
+                confirm = false;
+            }//end for
         }//end for
     }//end method main
 }//end class tester
