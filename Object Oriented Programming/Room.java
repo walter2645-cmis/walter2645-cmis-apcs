27baf7653bac591c45abc76577722ea13e60c69f
cmis <cmis@cmis-hs00.(none)>
2016-09-20 11:55:12 +0700

    Created object oriented programming.

---
+++ Object Oriented Programming/Room.java
@@ -0,0 +1,33 @@
+public class Room
+{
+    private String name;
+    
+    public static void main( String[] args )
+    {
+        Room r1 = new Room();
+        System.out.println(r1);
+    }//end method main
+}//end class Room
+/*
+ * Room
+
+ * Description
+ *  Width
+ *  Depth
+ *  Height
+ *  entrance/exit
+ *  Name
+ *  windows
+What it does
+    Store item(s)
+        w/d/h, entrance
+    increase/decrease size
+        w/d/h
+    add/remove entrance/exit
+        entrance/exit
+    install/remove windows
+        Windows
+    Used as meeting place for people
+        Name, entrance/exit 
+
+ */
