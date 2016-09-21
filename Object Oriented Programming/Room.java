a493191a55399fa3bfa5cc6a64c2ac56a8400239
cmis <cmis@cmis-hs00.(none)>
2016-09-21 14:35:51 +0700

    Started on constructor methods.

--- Object Oriented Programming/Room.java
+++ Object Oriented Programming/Room.java
@@ -1,11 +1,61 @@
+import javax.swing.JOptionPane;
 public class Room
 {
     private String name;
-    
+    private boolean entrance; //true -> has entrance, false -> no entrance
+    private int windows;
+    private double height;
+    private double width;
+    private double depth;
+    public Room( String name, int windows, boolean entrance )
+    {
+        this.name = name;
+        this.windows = windows;
+        this.entrance = entrance;
+    }//end method room
+    public Room( String name )
+    {
+        this.name = name;
+        double ent = Math.random();
+        if( ent > 0.5 )
+        {
+            this.entrance = true;
+        }//end if
+        else
+        {
+            this.entrance = false;
+        }//end else
+        this.height = Math.random() * 50;
+        this.width = height * 2;
+        this.depth = height * Math.random() * 10;
+    }//end method room
+    public Room( String name, String password )
+    {
+        this.name = name;
+        if( password == "CIEL TAMER" )
+        {
+            this.entrance = true;
+        }//end if
+    }//end method room
     public static void main( String[] args )
     {
-        Room r1 = new Room();
+        Room r1 = new Room("HS302", 24, true);
         System.out.println(r1);
+        System.out.println(r1.name);
+        System.out.println(r1.windows);
+        System.out.println(r1.entrance);
+        
+        Room r2 = new Room("Radomus's Room");
+        System.out.println(r2);
+        System.out.println(r2.name);
+        System.out.println(r2.entrance);
+        System.out.println("h = " + r2.height);
+        System.out.println("w = " + r2.width);
+        System.out.println("d = " + r2.depth);
+        
+        String pass = JOptionPane.showInputDialog("Currently in use.\n    -T3RR4");
+        Room r3 = new Room("T3RR4's Room", pass);
+        System.out.println(r3 + "\n" + r3.name + "\n" + r3.entrance);
     }//end method main
 }//end class Room
 /*
