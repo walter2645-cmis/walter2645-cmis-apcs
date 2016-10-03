e732d30c1cea06516ab676acef50dd540c76aee2
cmis <cmis@cmis-hs02.(none)>
2016-10-03 14:37:27 +0700

    Created Student Information System.

---
+++ Student Information System/Student.java
@@ -0,0 +1,24 @@
+
+public class Student{
+  //FIELDS
+  //class variables
+  
+  //instance variables
+  
+  //constants
+  
+  //METHODS
+  //constructors
+  public Student(){
+    
+  }
+  
+  //class (static) methods
+  
+  //getter/setter methods
+  
+  //other non-static methods
+  public String toString(){
+   return "";
+  }
+}
