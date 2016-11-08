31607ca814cfcb47ff02e6c1fbaa4f9cd8887e98
cmis <cmis@cmis-hs02.(none)>
2016-11-08 14:37:22 +0700

    Completed Problem3. Started Problem4.

--- 2D Arrays/Array2D3.java
+++ 2D Arrays/Array2D3.java
@@ -1,22 +1,33 @@
+import javax.swing.JOptionPane;
 public class Array2D3
 {
     public static void main( String[] args )
     {
-        
-        int[][] array = new int[9][9];
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Type a number.")) + 1;
+        int[][] array = new int[num][num];
         for( int y = 0; y < array.length; y++ )
         {
             for( int x = 0; x < array[0].length; x++ )
             {
-                array[0][x] = x;
-                array[y][0] = y;
+                if( y == 0 )
+                {
+                    array[0][x] = x;
+                }//end if
+                else if( x == 0 )
+                {
+                    array[y][0] = y;
+                }//end else if
+                else
+                {
+                    array[y][x] = x * y;
+                }//end else
             }//end for
         }//end for
         for( int y = 0; y < array.length; y++ )
         {
             for( int x = 0; x < array[0].length; x++ )
             {
-                System.out.print(array[y][x]);
+                System.out.format("%-9d", array[y][x]);
             }//end for
             System.out.print("\n");
         }//end for
---
+++ 2D Arrays/Array2D4.java
@@ -0,0 +1,10 @@
+public class Array2D4
+{
+    public static void main( String[] args )
+    {
+        char[][] array = new char[28][28];
+        char y = 97;
+        System.out.println(y);
+    }//end method main
+}//end class Array2D4
+//https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
