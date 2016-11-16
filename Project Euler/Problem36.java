d98f3929d7fec0b0f228d431a79db2fdca831772
cmis <cmis@cmis-hs02.(none)>
2016-11-16 10:35:50 +0700

    Completed Problem4 later than Joy. =.=' Started Problem36.

---
+++ Project Euler/Problem36.java
@@ -0,0 +1,8 @@
+public class Problem36
+{
+    public static void main( String[] args )
+    {
+        int num = 10000000;
+        //binary converter is in fundamentals
+    }//end method main
+}//end class Problem36
---
+++ Project Euler/Problem4.java
@@ -0,0 +1,49 @@
+public class Problem4
+{
+    public static void main( String[] args )
+    {
+        int[][] array = new int[999][999];
+        int big = 0;
+        for( int y = array.length - 1; y > 1; y-- )
+        {
+            for( int x = array[0].length - 1; x > 1; x-- )
+            {
+                array[y][x] = (y + 1) * (x + 1);
+                String l = Integer.toString(array[y][x]);
+                char[] num = ("" + array[y][x]).toCharArray();
+                int zero = 0;
+                int i = num.length - 1;
+                boolean ayy = true;
+                while( ayy )
+                {
+                    if( zero > i )
+                    {
+                        ayy = false;
+                        if( array[y][x] > big )
+                        {
+                            big = array[y][x];
+                        }//end if
+                        x -= array[0].length;
+                    }//end if
+                    else if( zero == i - 1 && num[zero] == num[i] )
+                    {
+                        ayy = false;
+                        if( array[y][x] > big )
+                        {
+                            big = array[y][x];
+                        }//end if
+                        x -= array[0].length;
+                    }//end else if
+                    else if( num[zero] != num[i] )
+                    {
+                        ayy = false;
+                    }//end else if
+                    zero++;
+                    i--;
+                }//end while
+
+            }//end for
+        }//end for
+        System.out.println( big );
+    }//end method main
+}//end class Problem4
