ddbeffa7d8de776a8046be29b0c7918f7c2f3d6c
cmis <cmis@cmis-hs02.(none)>
2016-11-18 14:36:38 +0700

    Bashed my head against the wall of problem6.

--- 2D Arrays/Array2D6.java
+++ 2D Arrays/Array2D6.java
@@ -5,11 +5,11 @@ public class Array2D6
     {
         String name = JOptionPane.showInputDialog("Please enter your last name.");
         String pw = JOptionPane.showInputDialog("Please enter your new password.");
-        String pwCheck = pw.toLowerCase();
-        String nameCheck = name.toLowerCase();
         boolean forbidden = true;
         while( forbidden )
         {
+            String pwCheck = pw.toLowerCase();
+            String nameCheck = name.toLowerCase();
             char[] nameChar = nameCheck.toCharArray();
             char[] pwChar = pwCheck.toCharArray();
             if( pw.length() < name.length() || pw.length() == name.length() && !pwCheck.equals(nameCheck) )
@@ -18,13 +18,8 @@ public class Array2D6
             }//end if
             else if( pw.length() > name.length() )
             {
-                int diff = pw.length() - name.length();
-                while( end < pw.length )
-                {
-                    char[] pwSegment = new char[name.length];
-                }//end while
+                
             }//end else if
-            
         }//end while
     }//end method main
 }//end class
