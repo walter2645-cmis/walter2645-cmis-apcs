a9648ca006f4e0de80ae102d9b1815b4072f124d
cmis <cmis@cmis-hs02.(none)>
2017-01-17 14:37:37 +0700

    tutorial.

---
+++ modern-crab/CrabWorld.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CrabWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CrabWorld extends World
+{
+
+    /**
+     * Constructor for objects of class CrabWorld.
+     * 
+     */
+    public CrabWorld()
+    {    
+        super(560, 560, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
+}
