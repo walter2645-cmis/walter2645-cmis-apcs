2d54bedb1d64f3097263df9bb6ccde4ab89624e4
cmis <cmis@cmis-hs02.(none)>
2017-01-19 09:57:27 +0700

    Started on movement problems

--- Movement/Darude.java
+++ Movement/Darude.java
@@ -3,17 +3,38 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Darude here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
+
 public class Darude extends Actor
 {
+    private int decision = Greenfoot.getRandomNumber(4);
     /**
      * Act - do whatever the Darude wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public void act()
     {
-        
-    }    
+        if( Greenfoot.getRandomNumber(100) < 7 )
+        {
+            decision = Greenfoot.getRandomNumber(4);
+        }
+        if( decision == 0 )
+        {
+            move(-4);
+        }//end if
+        else if( decision == 1 )
+        {
+            move(4);
+        }//end else if
+        else if( decision == 2 )
+        {
+            setLocation(getX(), getY()-4);
+        }//end else if
+        else if( decision == 3 )
+        {
+            setLocation(getX(), getY()+4);
+        }//end else if
+    }
 }
