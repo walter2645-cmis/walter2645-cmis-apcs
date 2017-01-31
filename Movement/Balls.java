37cde376d89e08e51ba832ec3b5fbe622c497a9b
cmis <cmis@cmis-hs02.(none)>
2017-01-31 08:40:44 +0700

    Tried to make balls bounce at an angle. Balls were overattached to walls.

--- Movement/Balls.java
+++ Movement/Balls.java
@@ -16,16 +16,34 @@ public class Balls extends Actor
     {
         turn(Greenfoot.getRandomNumber(360));
     }
+
     public void act() 
     {
         if( isAtEdge() || isTouching(null) )
         {
-            turn(Greenfoot.getRandomNumber(180) - 90);
-            move(4);
-        }
-        else
+            //turn(Greenfoot.getRandomNumber(180) - 90);
+            //move(4);
+        //}
+        /*if (getY() == 0 || getY() == getWorld().getHeight()-1)
         {
-            move(3);
+            setRotation(360-getRotation());
         }
+        else if (getX()==0 || getX() == getWorld().getWidth()-1)
+        {
+            setRotation(180-getRotation());
+        }*/
+        int a = getRotation();
+        if( (a < 45 || a > 315) || (a > 135 && a < 225) )
+        {
+            setRotation(179-getRotation());
+        }
+        if( (a >= 45 && a <= 135) || (a >= 225 && a <= 315) )
+        {
+            setRotation(359-getRotation());
+        }
+    }
+        //{
+            move(3);
+        //}
     } 
 }
--- Movement/MyWorld.java
+++ Movement/MyWorld.java
@@ -25,7 +25,7 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        populate3();
+        populate2();
     }
     public void populate()
     {
