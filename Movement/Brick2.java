aaec12a8d4163d24e103f08113e764559fd29771
cmis <cmis@cmis-hs02.(none)>
2017-02-06 08:42:41 +0700

    Mexicans were deported. The wall is still holey.

--- Movement/Brick2.java
+++ Movement/Brick2.java
@@ -8,81 +8,57 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Brick2 extends Actor
 {
-    private boolean borderLeft = true;
-    private boolean borderRight = true;
-    private boolean borderUp = true;
-    private boolean borderDown = true;
     /**
      * Act - do whatever the Brick wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        //Mexican();
-        if( Greenfoot.isKeyDown("a") && borderLeft )
+        if( Greenfoot.isKeyDown("a") )
         {
-            move(-4);
+            if( isTouching( Trump.class ) )
+            {
+                move(1);
+            }
+            else
+            {
+                move(-1);
+            }
         }//end if
-        if( Greenfoot.isKeyDown("d") && borderRight )
+        if( Greenfoot.isKeyDown("d") )
         {
-            move(4);
+            if( isTouching( Trump.class ) )
+            {
+                move(-1);
+            }
+            else
+            {
+                move(1);
+            }
         }//end if
-        if( Greenfoot.isKeyDown("w") && borderUp )
+        if( Greenfoot.isKeyDown("w") )
         {
-            setLocation(getX(), getY()-4);
+            if( isTouching( Trump.class ) )
+            {
+                setLocation(getX(), getY()+1);
+            }
+            else{
+                setLocation(getX(), getY()-1);
+            }
         }//end if
-        if( Greenfoot.isKeyDown("s") && borderDown)
+        if( Greenfoot.isKeyDown("s") )
         {
-            setLocation(getX(), getY()+4);
+            if( isTouching( Trump.class ) )
+            {
+                setLocation(getX(), getY()-1);
+            }
+            else
+            {
+                setLocation(getX(), getY()+1);
+            }
         }//end if
         worldEnd();
     }
-    
-    
-    
-    /*public void Mexican()
-    {
-        boolean upLeft = getOneObjectAtOffset( -25, -25, Trump.class ) != null;
-        boolean downLeft = getOneObjectAtOffset( -25, 25, Trump.class ) != null;
-        boolean upRight = getOneObjectAtOffset( 25, -25, Trump.class ) != null;
-        boolean downRight = getOneObjectAtOffset( 25, 25, Trump.class ) != null;
-        boolean left = getOneObjectAtOffset( -25, 0, Trump.class ) != null;
-        boolean right = getOneObjectAtOffset( 25, 0, Trump.class ) != null;
-        boolean up = getOneObjectAtOffset( 0, -25, Trump.class ) != null;
-        boolean down = getOneObjectAtOffset( 0, 25, Trump.class ) != null;
-        if( upLeft || downLeft || left )
-        {
-            borderLeft = false;
-        }
-        else
-        {
-            borderLeft = true;
-        }
-        if( upRight || downRight || right )
-        {
-            borderRight = false;
-        }
-        else
-        {
-            borderRight = true;
-        }
-        if( downLeft || downRight || down )
-        {
-            borderDown = false;
-        }
-        else
-        {
-            borderDown = true;
-        }
-        if( upLeft || upRight || up )
-        {
-            borderUp = false;
-        }
-        else
-        {
-            borderUp = true;
-        }
-    }*/
 
     public void worldEnd()
     {
