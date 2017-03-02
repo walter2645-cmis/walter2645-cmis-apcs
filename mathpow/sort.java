411e27646aa1b9f19bbcc0d882c148402071e9d8
cmis <cmis@cmis-hs07.(none)>
2017-03-02 08:42:24 +0700

    Finished part 1 of sort.

---
+++ mathpow/sort.java
@@ -0,0 +1,72 @@
+
+/**
+ * Write a description of class sort here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class sort
+{
+    public static void main( String[] args )
+    {
+        int[] nums = new int[] {3, 3, 5, 8, 1, 6, 9, 4, 2, 7};
+        int[] ss = sort2(nums);
+        for( int i = 0; i < nums.length; i++ )
+        {
+            //nums[i] = (int)(Math.random() * 10);
+            System.out.print(ss[i] + " ");
+        }
+    }
+
+    public static int[] sort( int[] nums )
+    {
+        int min, cur, max;
+        min = Integer.MAX_VALUE;
+        cur = -1;
+        int[] sorted = new int[nums.length];
+        for( int i = 0; i < sorted.length; i++ )
+        {
+            for( int in = 0; in < nums.length; in++ )
+            {
+                if( nums[in] < min )
+                {
+                    min = nums[in];
+                    cur = in;
+                }
+            }
+            sorted[i] = min;
+            min = Integer.MAX_VALUE;
+            nums[cur] = Integer.MAX_VALUE;
+        }
+        return sorted;
+    }
+
+    public static int[] sort2( int[] nums )
+    {
+        int min, cur, first, dawg;
+        min = Integer.MAX_VALUE;
+        cur = -1;
+        first = Integer.MIN_VALUE;
+
+        for( int i = 0; i < nums.length; i++ )
+        {
+            for( int in = 0; in < nums.length; in++ )
+            {
+                if( nums[in] < min && nums[in] >= first )
+                {
+                    min = nums[in];
+                    cur = in;
+                }
+            }
+            if( nums[i] != nums[cur] )
+            {
+                dawg = nums[i];
+                nums[i] = nums[cur];
+                nums[cur] = dawg;
+                first = min;
+                min = Integer.MAX_VALUE;
+            }
+        }
+        return nums;
+    }
+}
