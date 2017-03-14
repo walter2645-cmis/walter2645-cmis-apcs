156317fe22a2b5251756efd579d7115d848df544
cmis <cmis@cmis-hs06.(none)>
2017-03-14 14:36:55 +0700

    Started mergesort.

---
+++ mathpow/merge.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class merge here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class merge
+{
+    public static void main( String[] args )
+    {
+        int a = 3;
+        int b = 2;
+        int c = a/b;
+        System.out.println(c);
+    }
+    public static int[][] split(int[] cut)
+    {
+        int mid = cut.length / 2;
+        int[][];
+        return null;
+    }
+    public static int[] mergeSort(int[] juan)
+    {
+        if(juan.length == 1)
+        {
+            return juan;
+        }
+        else
+        {
+            int[][] duality = split(juan);
+        }
+        return null;
+    }
+    public static int[] merge()
+    {
+        return null;
+    }
+}
