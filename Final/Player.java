71560190620e4d277e24b29b0c1fcdfcb18fd0e5
cmis <cmis@cmis-hs08.(none)>
2017-05-11 11:55:47 +0700

    Changes were made.

--- Final/Player.java
+++ Final/Player.java
@@ -1,17 +1,41 @@
 import greenfoot.*;
+import java.awt.Color;
 public class Player extends CanMove implements HP
 {
     private int HP;
+    private GreenfootImage img;
+    public Player()
+    {
+        img = new GreenfootImage(25, 25);
+        img.setColor(new Color(255, 51, 255));
+        img.fill();
+        setImage(img);
+    }
     public void takeDamage(int damage)
     {
         
     }
     public void movement(int speed)
     {
-        
+        if( Greenfoot.isKeyDown("a") )
+        {
+            move(-speed);
+        }//end if
+        if( Greenfoot.isKeyDown("d") )
+        {
+            move(speed);
+        }//end if
+        if( Greenfoot.isKeyDown("w") )
+        {
+            setLocation(getX(), getY()-speed);
+        }//end if
+        if( Greenfoot.isKeyDown("s") )
+        {
+            setLocation(getX(), getY()+speed);
+        }//end if
     }
     public void act() 
     {
-        
+        movement(3);
     }    
 }
--- Final/Projectile.java
+++ Final/Projectile.java
@@ -1,12 +1,22 @@
 import greenfoot.*;
+import java.awt.Color;
 public class Projectile extends CanMove
 {
+    private GreenfootImage img;
+    public Projectile(int x, int y, Color color)
+    {
+        img = new GreenfootImage(20, 3);
+        img.setColor(color);
+        img.fill();
+        setImage(img);
+        turnTowards(x, y);
+    }
     public void movement(int speed)
     {
         move(speed);
     }
     public void act() 
     {
-        
-    }    
+        movement(5);
+    }
 }
